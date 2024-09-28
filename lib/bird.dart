7d6b64d9a1962b6592ae2b21f6a00a3943d50a20
deepanitashah <deepanitashah2711@gmail.com>
2024-09-28 13:34:12 +0530

    homepage and bird

---
+++ lib/bird.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+  const MyBird({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      width: 120,
+      child: Image.asset(
+        'assets/flappy.png'
+      ),
+    );
+  }
+}
